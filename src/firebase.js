e4d16fdad4ba59bd5560684153cf0930d34439a3
johng <jcguest90@gmail.com>
2020-08-22 18:57:48 -0500

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBiZm1yWUQqvDYjH-CLwWSqT-r5bNTIv8",
+    authDomain: "whatsapp-clone-971ab.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-971ab.firebaseio.com",
+    projectId: "whatsapp-clone-971ab",
+    storageBucket: "whatsapp-clone-971ab.appspot.com",
+    messagingSenderId: "4405421351",
+    appId: "1:4405421351:web:6e53a2385ae0d1abe57502",
+    measurementId: "G-356412ZLHK"
+  };
