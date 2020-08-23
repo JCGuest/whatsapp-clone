ba17c3804b26752b338f24add1902991506d7781
johng <jcguest90@gmail.com>
2020-08-23 00:02:47 -0500

    chats cursor and hover

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -4,14 +4,14 @@ import { Avatar } from "@material-ui/core";
 
 const SidebarChat = () => {
 
-    const [seed, setSeed] = useState()
+    const [seed, setSeed] = useState("")
 
     useEffect( () => {
         setSeed(Math.floor(Math.random() * 5000));
     }, [])
 
     return (
-        <div className="sidbarChat">
+        <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>
             <div className="sidbarChat__info">
                 <h2>Room Name</h2>
