60a9b2a5293c77c06e56905c079a61de7fd94ea5
johng <jcguest90@gmail.com>
2020-08-25 22:21:33 -0500

    router, sidebar outside of route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/App.css';
 import Sidebar from './components/Sidebar.js'
 import Chat from './components/Chat.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -9,8 +10,23 @@ function App() {
     <div className="app">
       <h1>WhatsReactApp</h1>
         <div className="app__body">
-          <Sidebar />
-          <Chat/>
+          <Router>
+              <Switch>
+              <Sidebar/>
+            
+                <Route path="/">
+                  <Chat/>
+                </Route>
+                
+                <Route path="/rooms/:roomId">
+                  <Chat/>
+                </Route>
+
+              </Switch>
+          </Router> 
+
+
+          
         </div>
     </div>
   );
