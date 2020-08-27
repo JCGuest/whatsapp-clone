d12206742537202525aadf0cd689aef392786f0b
johng <jcguest90@gmail.com>
2020-08-27 01:43:00 -0500

    reducer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
-import Sidebar from './components/Sidebar.js'
-import Chat from './components/Chat.js'
+import Sidebar from './components/Sidebar.js';
+import Chat from './components/Chat.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './components/Login.js';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     // BEM naming convention "app"
     <div className="app">
-      <h1>WhatsReactApp</h1>
+      {!user? (
+        <Login/>
+      ): (
         <div className="app__body">
-          <Router>
-            <Sidebar/>
-              <Switch>
-                <Route path="/rooms/:roomId">
-                  <Chat/>
-                </Route>
-                <Route exact path='/'>
-                  <Chat/>
-                </Route>
-              </Switch>
-          </Router> 
-
-
-          
-        </div>
+        <Router>
+          <Sidebar/>
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat/>
+              </Route>
+              <Route exact path='/'>
+                <Chat/>
+              </Route>
+            </Switch>
+        </Router> 
+      </div>
+      )}
     </div>
   );
 }
